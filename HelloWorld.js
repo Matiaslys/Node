cfefce71e4802a06f75f24ddb5ea80bb655f4cae
Matias <54976149+Matiaslys@users.noreply.github.com>
2020-09-09 10:38:46 +0200

    W3schools fileSystem

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,9 +1,13 @@
 var http = require('http')
 var dt = require('./myfirstmodule')
+var fs = require('fs')
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write("Hello World! ")
-    res.write("The date and time are currently: " + dt.myDateTime())
-    res.end()
+    fs.readFile('demofile.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write(data)
+        res.write("Hello World! ")
+        res.write("The date and time are currently: " + dt.myDateTime())
+        res.end()
+    })
 }).listen(8080)
